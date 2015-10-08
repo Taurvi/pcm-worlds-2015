4856b975ef8e5e2437ca5a8ab3e4723da25e32cb
Taurvi <srimbakusumo@gmail.com>
2015-10-08 01:14:03 -0700

    added basic functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-/**
- * Created by taurvi on 10/7/15.
- */
+$(window).scroll(function() {
+    // ?????????? 
+    // Get scroll position
+    var s = $(window).scrollTop(),
+    // ???????????
+    // scroll value and opacity
+        opacityVal = (s / 250.0);
+    // blur????????0%??100?
+    // opacity value 0% to 100%
+    $('.img-blur').css('opacity', opacityVal);
+});
