db835df1be3abd67781ac810d35529e53b937989
Stephen Rimbakusumo <srimbakusumo@gmail.com>
2015-10-09 22:28:21 -0700

    Added more content and a nav bar

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ $(window).scroll(function() {
     // opacity value 0% to 100%
     $('.img-blur').css('opacity', opacityVal);
 });
+
+function scrollToElement (selector) {
+    $('html, body').animate({
+        scrollTop: $(selector).offset().top
+    }, 2000);
+};
+
+$(document).on('click', 'a.uruna', function () {
+    scrollToElement($(this).attr('href'));
+});
